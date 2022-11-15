ebbb7c6fb473d65d3fb713aa0dc1ca65224bc573
onebitjoy <vinaykrkharwar@gmail.com>
2022-11-15 19:06:31 +0530

    New implementations - 1 - Entire theming around material theme 3 2 - Added Round Off switch and implemented 3 - Given more spacing between the TextFields

--- app/src/main/java/com/example/tip/MainActivity.kt
+++ app/src/main/java/com/example/tip/MainActivity.kt
@@ -21,7 +21,14 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.tip.ui.theme.TipTheme
+import java.text.NumberFormat
 
+/*
+* Name - Vinay Kumar Kharwar
+* Github - https://www.github.com/onebitjoy
+* Title - Tip Calculator
+* Updated latest on - 15th November, 2022 / 19:01
+* */
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -48,20 +55,24 @@ fun TipPreview() {
 @Composable
 fun MainScreen() {
     // User Input Changing Values: Remember by observables
-    var amountInput by remember { mutableStateOf("") }
-    var tipInput by remember { mutableStateOf("") }
+    var amountInput by remember { mutableStateOf("") } // Amount entered
+    var tipInput by remember { mutableStateOf("") } // Tip percentage entered
+    var checked by remember { mutableStateOf(false) } // Switch on/off
+
 
     // Variable: Watching Variable for the observables
-    val amount = amountInput.toDoubleOrNull() ?: 0.0
+    val amount = amountInput.toDoubleOrNull() ?: 0.0 // convert amountInput to Double and then calculate tip
     val tipvalue = tipInput.toDoubleOrNull() ?: 15.0 // Default Tip Percentage = 15%
 
-    val tip: Double = tipCalc(amount, tipvalue)
+    val tip: Double = tipCalc(amount, tipvalue, checked)
     val focusManager = LocalFocusManager.current
 
-    Box(modifier = Modifier
-        .fillMaxSize()
-        .background(Color(0xFFABC3E7))
-        .padding(top = 100.dp)) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xFFFDFAEC))
+            .padding(top = 100.dp)
+    ) {
         Column(
             modifier = Modifier
                 .fillMaxSize()
@@ -77,17 +88,33 @@ fun MainScreen() {
             Spacer(modifier = Modifier.height(50.dp))
 
 
-            NumberField(amountInput, onValueChanged = { amountInput = it }, ImeAction.Next, "Amount",KeyboardActions(
-                onNext = { focusManager.moveFocus(FocusDirection.Down) }
-            ))
-            Spacer(modifier = Modifier.height(15.dp))
+            NumberField(
+                amountInput,
+                onValueChanged = { amountInput = it },
+                ImeAction.Next,
+                "Amount",
+                KeyboardActions(onNext = { focusManager.moveFocus(FocusDirection.Down) })
+            )
+            Spacer(modifier = Modifier.height(30.dp))
 
-            NumberField(tipInput, onValueChanged = { tipInput = it }, ImeAction.Done, "Tip Percentage",KeyboardActions(
-                onDone = { focusManager.clearFocus() }
-            ))
+            NumberField(
+                tipInput,
+                onValueChanged = { tipInput = it },
+                ImeAction.Done,
+                "Tip Percentage",
+                KeyboardActions(onDone = { focusManager.clearFocus() })
+            )
             Spacer(modifier = Modifier.height(70.dp))
 
-            Text(text = stringResource(id = R.string.show, tip), fontSize = 36.sp)
+            RoundTheTip(roundUp = checked, onRoundUpChanged = { checked = it })
+            Spacer(modifier = Modifier.height(15.dp))
+
+            Text(
+                text = stringResource(
+                    id = R.string.show,
+                    NumberFormat.getCurrencyInstance().format(tip)
+                ), fontSize = 36.sp
+            )
         }
     }
 
@@ -99,15 +126,14 @@ fun NumberField(
     amountInput: String,
     onValueChanged: (String) -> Unit,
     imicon: ImeAction = ImeAction.Next,
-    label:String,
+    label: String,
     keyboardActions: KeyboardActions
 ) {
     Card(
-        elevation = 5.dp,
-        backgroundColor = Color(0xFFD0E6F0)
+        elevation = 5.dp, backgroundColor = Color(0xFFFCF6D8)
     ) {
         TextField(
-            placeholder = {Text(text = label, fontSize = 24.sp, color = Color(0xFF656865))},
+            placeholder = { Text(text = label, fontSize = 24.sp, color = Color(0xFF656865)) },
             value = amountInput,
             onValueChange = onValueChanged,
             singleLine = true,
@@ -117,8 +143,7 @@ fun NumberField(
             textStyle = MaterialTheme.typography.h3,
             keyboardActions = keyboardActions,
             keyboardOptions = KeyboardOptions.Default.copy(
-                keyboardType = KeyboardType.Number,
-                imeAction = imicon
+                keyboardType = KeyboardType.Number, imeAction = imicon
             ),
             colors = TextFieldDefaults.textFieldColors(
                 textColor = Color.Black,
@@ -131,9 +156,39 @@ fun NumberField(
 
 }
 
+// Switch Function
+@Composable
+fun RoundTheTip(roundUp: Boolean, onRoundUpChanged: (Boolean) -> Unit) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .size(60.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.End
+    ) {
+        Text(text = "Round Off?", fontSize = 24.sp)
+        Switch(
+            checked = roundUp,
+            onCheckedChange = onRoundUpChanged,
+            modifier = Modifier
+                .wrapContentWidth(Alignment.End)
+                .padding(start = 5.dp),
+            colors = SwitchDefaults.colors(
+                uncheckedThumbColor = Color(0xFF59C1F1),
+                uncheckedTrackColor = Color(0xFFABE4EE),
+                checkedTrackColor = Color(0xFFF52727),
+                checkedThumbColor = Color(0xFFF80026),
+            )
+        )
+    }
+}
+
 //Tip Function
-internal fun tipCalc(amount: Double, tipvalue: Double = 15.0): Double {
-    return (tipvalue * amount) / 100
+internal fun tipCalc(amount: Double, tipvalue: Double = 15.0, roundOff: Boolean = false): Double {
+    var tipped = tipvalue / 100 * amount
+    if (roundOff)
+        tipped = kotlin.math.ceil(tipped)
+    return tipped
 }
 
 //Committed
